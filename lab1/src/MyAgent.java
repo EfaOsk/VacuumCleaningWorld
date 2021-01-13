e47aef105511181b2aeedc339d8cc161037b2e7f
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2021-01-13 15:42:28 +0000

    ZicZac- fixed

--- lab1/src/MyAgent.java
+++ lab1/src/MyAgent.java
@@ -5,27 +5,40 @@ public class MyAgent implements Agent
 	boolean isTurnedOn = false;
 	int posY = 0, posX = 0, ori = 0;
     public String nextAction(Collection<String> percepts) {
-		System.out.print("perceiving:");
-		for(String percept:percepts) {
-			System.out.print("'" + percept + "', ");
-		}
-		System.out.println("");
-		String[] actions = { "TURN_ON", "TURN_OFF", "TURN_RIGHT", "TURN_LEFT", "GO", "SUCK" };
-		
+		// System.out.print("perceiving:");
+		// for(String percept:percepts) {
+		// 	System.out.print("'" + percept + "', ");
+		// }
+		// System.out.println("");
+		// String[] actions = { "TURN_ON", "TURN_OFF", "TURN_RIGHT", "TURN_LEFT", "GO", "SUCK" };
+		System.out.print(String.format("X: %d Y: %d (ori: %d)\n", posX, posY, ori));
 		if (!isTurnedOn) {
 			isTurnedOn = true;
 			return "TURN_ON";
 		} else if (percepts.contains("DIRT")){
 			return "SUCK";
 		} else if (percepts.contains("BUMP")) {
-			if (ori == 0){ 	return "TURN_RIGHT"; }
+			if (ori == 0){ 	
+				ori = (ori+1)%4;
+				return "TURN_RIGHT"; }
 			else if (ori == 1){ 	return "TURN_OFF"; } // GO home (But not nesseraly)
-			else if (ori == 2){ 	return "TURN_LEFT"; }
+			else if (ori == 2){ 	
+				ori = (ori-1)%4;
+				return "TURN_LEFT"; }
 			else { 	return "TURN_OFF"; } // GO home
 		} else if (ori == 1){
-			return "TURN_RIGHT";
-		} else if (ori == 3){
-			return "TURN_LEFT";
+			if (posY == 0){	
+				if (posX%2 == 0){	
+					System.out.print(String.format("%d)\n", posX%2));
+					ori = (ori-1)%4;
+					return "TURN_LEFT";
+				} else {
+				return go(); }
+			}
+			if (posX%2 == 0){	return go(); } 
+			else {
+				ori = (ori+1)%4;
+				return "TURN_RIGHT"; }
 		} else {
 			return go();
 		}
