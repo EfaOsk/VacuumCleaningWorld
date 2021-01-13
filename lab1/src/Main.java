e0beaf3621ad1765cd2cbb89e815095a0048d5cc
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2021-01-13 22:05:48 +0000

    Added Hólmfríððurs Agent
    Note, dose not work corectly

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args){
 		try{
 			// TODO: put in your agent here
-			Agent agent = new MyAgent();
+			Agent agent = new MyAgent1();
 
 			int port=4001;
 			if(args.length>=1){
