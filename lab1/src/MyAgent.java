d4ea5f5a63e525b2220b35582592631137d9fa78
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2021-01-13 23:16:03 +0000

    Now Agent, cleans the whole aria

--- lab1/src/MyAgent.java
+++ lab1/src/MyAgent.java
@@ -2,8 +2,9 @@ import java.util.Collection;
 
 public class MyAgent implements Agent
 {
-	boolean isTurnedOn = false, isFinished= false;
+	boolean isTurnedOn = false, isFinished= false, isStarted = false;
 	int posY = 0, posX = 0, ori = 0;
+	int minY = 0;
     public String nextAction(Collection<String> percepts) {
 		// System.out.print("perceiving:");
 		// for(String percept:percepts) {
@@ -16,9 +17,25 @@ public class MyAgent implements Agent
 			if (!isTurnedOn) {
 				isTurnedOn = true;
 				return "TURN_ON";
+			} else if (!isStarted) {
+				if (ori == 0) { return turnLeft(); }
+				else if (ori == 3) {
+					if (percepts.contains("BUMP")) { goBack(); return turnLeft(); }
+					return go();
+				} else if (ori == 2) {
+					if (percepts.contains("BUMP")) { 
+						goBack();
+						minY = posY;
+						return turnLeft(); }
+					return go();
+				} else { 
+					isStarted = true;
+					return turnLeft();
+				}
 			} else if (percepts.contains("DIRT")){
 				return "SUCK";
 			} else if (percepts.contains("BUMP")) {
+				goBack(); 
 				if (ori == 0){ return turnRight(); }
 				else if (ori == 1){ 
 					isFinished = true;
@@ -28,7 +45,7 @@ public class MyAgent implements Agent
 					isFinished = true;
 					return goHome(); } // GO home
 			} else if (ori == 1){
-				if (posY == 0){	
+				if (posY == minY){	
 					if (posX%2 == 0){ return turnLeft();
 					} else { return go(); }
 				}
@@ -58,26 +75,51 @@ public class MyAgent implements Agent
 		}
 		return "GO";
 	}
+	public void goBack() {
+        switch (ori) {
+            case 0:
+				posY--;
+				break;
+			case 1:
+				posX--;
+				break;
+			case 2:
+				posY++;
+				break;
+			case 3:
+				posX++;
+				break;
+		}
+    }
 	public String turnRight() {
 		ori = (ori+1)%4;
 		return "TURN_RIGHT";
 	}
 	public String turnLeft() {
-		ori = (ori-1)%4;
+		ori = (ori-1+4)%4;
 		return "TURN_LEFT";
 	}
 	public String goHome() {
 		if ((posX == 0) && (posY == 0)){
 			return "TURN_OFF";
 		} else {
-			if (ori == 0){ return turnLeft(); }
-			else if (ori == 1){ return turnRight(); }
-			else if (ori == 2){
-				if (posY >0) { return go(); }
-				return turnRight();
+			if (posY > 0) {
+				if (ori == 0){ return turnLeft(); }
+				else if (ori == 1){ return turnRight(); }
+				else if (ori == 2){ return go(); }
+				else {
+					if (posX > 0) { return go(); }
+					return turnLeft();
+				}
 			} else {
-				if (posX >0) { return go(); }
-				return turnLeft();
+				if (ori == 0){ return go(); }
+				else if (ori == 1){ return turnLeft(); }
+				else if (ori == 2){
+					return turnRight();
+				} else {
+					if (posX >0) { return go(); }
+					return turnRight();
+				}
 			}
 		}
 	}
